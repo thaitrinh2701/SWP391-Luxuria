36219dd5d7aabe01085032edf00fbdbf62e1dff6
Phạm Nguyên Vũ <vupnse184101@fpt.edu.vn>
2024-06-12 18:02:18 +0700

    Update document V3

--- Luxuria_FE/src/components/Card/index.js
+++
@@ -1,2 +0,0 @@
-export { Card } from "./Card";
-export { StatusIcon } from "./StatusIcon";
--- Luxuria_FE/src/components/Input/index.js
+++
@@ -1,3 +0,0 @@
-export { Input } from "./Input";
-export { Checkbox } from "./Checkbox";
-export { Select } from "./Select";
