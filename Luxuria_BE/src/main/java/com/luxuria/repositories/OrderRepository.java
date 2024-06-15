9829a15b7e3ea3dd98129ed0e4ba808c99dff2eb
LeVuDucLuong-SE183775 <LuongLVDSE183775@fpt.edu.vn>
2024-06-16 00:29:40 +0700

    fix sort bug

--- Luxuria_BE/src/main/java/com/luxuria/repositories/OrderRepository.java
+++ Luxuria_BE/src/main/java/com/luxuria/repositories/OrderRepository.java
@@ -2,8 +2,15 @@ package com.luxuria.repositories;
 
 import com.luxuria.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Order findByRequestId(Long requestId);
+
+    @Query("SELECT o FROM Order o WHERE o.request.user.id=:user_id")
+    List<Order> findAllByUserId(@Param("user_id") Long userId);
 }
--- Luxuria_BE/src/main/java/com/luxuria/repositories/RequestRepository.java
+++ Luxuria_BE/src/main/java/com/luxuria/repositories/RequestRepository.java
@@ -3,6 +3,7 @@ package com.luxuria.repositories;
 import com.luxuria.models.Request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,6 +11,6 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 
     List<Request> findByUserId(Long userId);
 
-    @Query("SELECT r from Request r WHERE r.isSalesStaffApproved = true")
-    List<Request> findApprovedRequestsByUserId(Long userId);
+    @Query("SELECT r from Request r WHERE r.isSalesStaffApproved = true AND r.user.id=:user_id")
+    List<Request> findApprovedRequestsByUserId(@Param("user_id") Long userId);
 }
